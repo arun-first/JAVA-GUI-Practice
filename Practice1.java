7750e5a19bc2b07ba2b4d875db319416562a7c6b
Arunoday <alpha-arun@outlook.com>
2021-11-10 01:36:16 +0530

    lecture 1 & 2

--- Practice1.java
+++ Practice1.java
@@ -3,10 +3,9 @@ import javax.swing.*;
 public class Practice1 {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Practice");
-		JButton btn = new JButton("Biwi");
 		frame.setVisible(true);
 		frame.setSize(400, 500);
-		frame.add(btn);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
---
+++ Practice2.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Practice2 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Frame 2");
+
+		frame.setSize(500,600);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
